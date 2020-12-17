f29227e5f09018e2cba26eb68e5c9d8d5b9b4e16
Максим Солуянов <maxide201@ya.ru>
2020-12-17 15:01:21 +0300

    update

--- general.js
+++ general.js
@@ -1,192 +1,179 @@
-let obj = JSON.parse(localStorage.getItem('obj'));
-if(!obj)
-	obj = [];
-function CreateExPage()
-{
-	addEx = document.createElement('div');
-	addEx.classList.add('addEx');
-	addEx.innerText = 'Add new exercise';
-	addEx.onclick = function(){
-		obj.unshift({"name":"exercise","days":[]});
-		localStorage.setItem('obj', JSON.stringify(obj));
-		window.open("date.html?id=0", "_self");
-	};
-	document.getElementById("content_block").append(addEx);
+let data = JSON.parse(localStorage.getItem('obj'));
+if(!data)
+	data = [];
+var pageType;
+var exId;
+var dayId;
 
-	for (var i = 0; i < obj.length; i++) 
+function AddDeleteButton(box, data_of_box, list)
+{
+	remove = document.createElement('img');
+	remove.src = 'trash.svg';
+	remove.classList.add('icon');
+	remove.onclick = function(){
+		list.splice(data_of_box.id, 1)
+		localStorage.setItem('data', JSON.stringify(data));
+		window.location = window.location.href;
+		data_of_box.isDeleting = true;
+	};
+	box.append(remove);
+}
+function AddBox(data_of_box, list)
+{
+	box = document.createElement('div');
+	box.classList.add('box');
+	box.innerText = list[data_of_box.id].name;
+	box.onclick = function(){
+		if(data_of_box.isDeleting == "false")
+			window.open(data_of_box.url, "_self");
+	};
+	AddDeleteButton(box, data_of_box, list);
+	document.getElementById("content_block").append(box);
+}
+function CreateExPage(list)
+{
+	header = document.createElement('div');
+	header.classList.add('header');
+	header.innerText = 'Добавить новое упражнение';
+	header.onclick = function(){
+		list.unshift({"name":"Упражнение","days":[]});
+		localStorage.setItem('data', JSON.stringify(data));
+		window.open("index.html?type=days&exId=0", "_self");
+	};
+	document.getElementById("content_block").append(header);
+
+	for (var i = 0; i < list.length; i++) 
 	{
-		let isDeleting = false;
-		let id = i;
-		box = document.createElement('div');
-		box.classList.add('box');
-		box.innerText = obj[i].name;
-		let url = "date.html?id="+i;
-		box.onclick = function(){
-			if(!isDeleting)
-				window.open(url, "_self");
-		};
-		remove = document.createElement('img');
-		remove.src = 'trash.svg';
-		remove.classList.add('trash');
-		remove.onclick = function(){
-			obj.splice(id, 1)
-			localStorage.setItem('obj', JSON.stringify(obj));
-			window.location = window.location.href;
-			isDeleting = true;
-		};
-		box.append(remove);
-		document.getElementById("content_block").append(box);
+		let data_of_box = {
+			"isDeleting":"false",
+			"id":i,
+			"url":("index.html?type=days&exId="+i)
+		}
+		AddBox(data_of_box, list);
 	}
 }
-
-function CreateDatePage()
+function CreateDaysPage(obj)
 {
-	var locString = document.location.search;
-	var searchParams = new URLSearchParams(locString);
-	var id = searchParams.get("id");
-
-	title = document.createElement('div');
-	title.classList.add('addEx');
-	title.innerText = obj[id].name;
-	title.onclick = function(){
-		const name = prompt("Input new title:");
+	header = document.createElement('div');
+	header.classList.add('header');
+	header.innerText = obj.name;
+	header.onclick = function(){
+		const name = prompt("Введите новое название:");
 		if(name)
 		{
-			obj[id].name = name;
-			localStorage.setItem('obj', JSON.stringify(obj));
-			let url = "date.html?id="+id;
-			window.open(url, "_self");
-
+			obj.name = name;
+			localStorage.setItem('data', JSON.stringify(data));
+			window.location = window.location.href;
 		}
 	};
 	pen = document.createElement('img');
 	pen.src = 'pen.svg';
-	pen.classList.add('trash');
-	title.append(pen);
-	document.getElementById("content_block").append(title);
+	pen.classList.add('icon');
+	header.append(pen);
+	document.getElementById("content_block").append(header);
 
 	addDay = document.createElement('div');
 	addDay.classList.add('box');
-	addDay.innerText = 'Add new workout';
+	addDay.innerText = 'Добавить новую тренировку';
 	addDay.onclick = function(){
-		let now = new Date();
-		let date_str = now.getDate()+'.'+now.getMonth()+'.'+now.getFullYear();
-		obj[id].days.unshift({"name":date_str,"sets":[]});
-		localStorage.setItem('obj', JSON.stringify(obj));
-		let url = "sets.html?day_id="+id+"&id=0";
+		let now = new Date(); let date_str = now.getDate()+'.'+now.getMonth()+'.'+now.getFullYear();
+		obj.days.unshift({"name":date_str,"sets":[]});
+		localStorage.setItem('data', JSON.stringify(data));
+		let url = "index.html?type=sets&exId="+exId+"&dayId=0";
 		window.open(url, "_self");
 	};
 	document.getElementById("content_block").append(addDay);
 
-	days = obj[id].days;
-	for (var i = 0; i < days.length; i++) 
+	for (var i = 0; i < obj.days.length; i++) 
 	{
-		let isDeleting = false;
-		let day_id = i;
-		box = document.createElement('div');
-		box.classList.add('box');
-		box.innerText = days[i].name;
-		let url = "sets.html?day_id="+id+"&id="+i;
-		box.onclick = function(){
-			if(!isDeleting)
-				window.open(url, "_self");
-		};
-		remove = document.createElement('img');
-		remove.src = 'trash.svg';
-		remove.classList.add('trash');
-		remove.onclick = function(){
-			days.splice(day_id, 1)
-			localStorage.setItem('obj', JSON.stringify(obj));
-			window.location = window.location.href;
-			isDeleting = true;
-		};
-		box.append(remove);
-		document.getElementById("content_block").append(box);
+		let data_of_box = {
+			"isDeleting":"false",
+			"id":i,
+			"url":("index.html?type=sets&exId="+exId+"&dayId="+i)
+		}
+		AddBox(data_of_box, obj.days);
 	}
-	back = document.createElement('div');
-	back.classList.add('back');
-	back.innerText = "back";
-	back.onclick = function(){
-		url = "index.html";
-		window.open(url, "_self");
-	};
-	document.getElementById("content_block").append(back);
 }
-
-function CreateSetsPage()
+function CreateSetsPage(obj)
 {
-	var locString = document.location.search;
-	var searchParams = new URLSearchParams(locString);
-	var id = searchParams.get("id");
-	var day_id = searchParams.get("day_id");
-
-	title = document.createElement('div');
-	title.classList.add('addEx');
-	title.innerText = obj[day_id].name;
-	document.getElementById("content_block").append(title);
+	header = document.createElement('div');
+	header.classList.add('header');
+	header.innerText = data[exId].name;
+	document.getElementById("content_block").append(header);
 
 	addSet = document.createElement('div');
 	addSet.classList.add('box');
-	addSet.innerText = 'Add new set';
+	addSet.innerText = 'Добавить подход';
 	addSet.onclick = function(){
-		obj[day_id].days[id].sets.unshift({"weight":"0", "times":"0"});
-		localStorage.setItem('obj', JSON.stringify(obj));
+		obj.sets.unshift({"weight":"0", "times":"0"});
+		localStorage.setItem('data', JSON.stringify(data));
 		window.location = window.location.href;
 	};
 	document.getElementById("content_block").append(addSet);
 
-	sets = obj[day_id].days[id].sets;
-	for (var i = 0; i < sets.length; i++) 
+	for (var i = 0; i < obj.sets.length; i++) 
 	{
-		isDeleting = false;
+		let number = obj.sets.length-i;
+		let id = i;
+		let data_of_box = {
+			"isDeleting":"false",
+			"id":i
+		}
 		box = document.createElement('div');
 		box.classList.add('box');
-		let number = sets.length-i;
-		box.innerText = "Set#"+(number)+"   weight: "+sets[number-1].weight+" times: "+ sets[number-1].times;
+		box.innerText = "Подход #"+(number)+"\nвес: "+obj.sets[i].weight+" кол-во: "+ obj.sets[i].times;
 		box.onclick = function(){
-			if(!isDeleting){
-				const weight = parseFloat(prompt("Input new weight:"));
-				const times = parseFloat(prompt("Input new times:"));
+			if(data_of_box.isDeleting == "false")
+			{
+				const weight = parseFloat(prompt("Введите вес:"));
+				const times = parseFloat(prompt("Введите кол-во:"));
 				if(weight && typeof(weight) != "NaN")
-					sets[number-1].weight = weight;
+					obj.sets[id].weight = weight;
 				if(times && typeof(times) != "NaN")
-					sets[number-1].times = times;
-				localStorage.setItem('obj', JSON.stringify(obj));
+					obj.sets[id].times = times;
+				localStorage.setItem('data', JSON.stringify(data));
 				window.location = window.location.href;
 			}
 		};
-		remove = document.createElement('img');
-		remove.src = 'trash.svg';
-		remove.classList.add('trash');
-		remove.onclick = function(){
-			sets.splice(number-1, 1)
-			localStorage.setItem('obj', JSON.stringify(obj));
-			window.location = window.location.href;
-			isDeleting = true;
-		};
-		box.append(remove);
+		AddDeleteButton(box, data_of_box, obj.sets);
 		document.getElementById("content_block").append(box);
 	}
-	back = document.createElement('div');
-	back.classList.add('back');
-	back.innerText = "back";
-	back.onclick = function(){
-		url = "date.html?id="+day_id;
-		window.open(url, "_self");
-	};
-	document.getElementById("content_block").append(back);
 }
-//alert(obj[0].name);
-//obj.unshift({"name":"ooo"});
-//alert(obj[0].name);
-//localStorage.setItem('ex', obj);
-//obj2 = JSON.parse(localStorage.getItem('ex'));
-//let json = JSON.stringify(obj);
+function Create()
+{
+	let locString = document.location.search;
+	let searchParams = new URLSearchParams(locString);
+	pageType = searchParams.get("type");
+	exId = searchParams.get("exId");
+	dayId = searchParams.get("dayId");
+
+	if(!pageType) CreateExPage(data);
+	else if(pageType == "days") CreateDaysPage(data[exId]);
+	else CreateSetsPage(data[exId].days[dayId]);
+
+	if(pageType)
+	{
+		back = document.createElement('div');
+		back.classList.add('back');
+		back.innerText = "back";
+		back.onclick = function(){
+			if(pageType == "days") url = "index.html";
+			else url = "index.html?type=days&exId="+exId;
+			window.open(url, "_self");
+		};
+		document.getElementById("content_block").append(back);
+	}
+}
+//alert(data[0].name);
+//data.unshift({"name":"ooo"});
+//alert(data[0].name);
+//localStorage.setItem('ex', data);
+//data2 = JSON.parse(localStorage.getItem('ex'));
+//let json = JSON.stringify(data);
 //alert(json);
 //alert(localStorage.getItem('first'));
 
-
-/*let obj = [
+/*let data = [
 	{
 		"name":"jumping",
 		"days":
